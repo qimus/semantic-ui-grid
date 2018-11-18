7e0cb04a6f73c194c64852691f03884d35605cd8
Denis Dyadyun <sysadm85@gmail.com>
2018-11-18 10:13:30 +0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { registerTypes as registerFilterTypes } from 'components/filters/factory'
 import ExtTable from './components'
+import AbstractProvider from './components/providers/AbstractProvider'
+import ApiDataProvider from './components/providers/ApiDataProvider'
+import ArrayDataProvider from './components/providers/ArrayDataProvider'
+
+const Provider = {
+    AbstractProvider,
+    ApiDataProvider,
+    ArrayDataProvider
+};
 
 export {
     registerFilterTypes,
-    ExtTable
+    ExtTable,
+    Provider
 }
