cf26b9d936075a0bc72f040d4e88ac7e80289e21
Denis Dyadyun <sysadm85@gmail.com>
2019-01-28 15:12:11 +0700

    add filter components

--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -10,6 +10,7 @@ import {
 
 import { factory } from './factory'
 import { ColumnOptions } from '../types'
+import {ReactNode} from "react";
 
 const styles = {
     form: {
@@ -30,15 +31,74 @@ interface FilterProps {
     itemsNames: any[];
     filterNamePrefix: string;
     options: ColumnOptions;
+    filterContainer: any;
+    filterContainerItem: any;
+    filterButtonComponent: any;
     filterButton?: {
         icon: string;
         visible: boolean;
     }
 }
 
+interface FilterContainerProps {
+    children: ReactNode;
+}
+
+interface FilterContainerItemProps {
+    children: ReactNode;
+    [prop: string]: any;
+}
+
+interface FilterButtonComponentProps {
+    icon?: string;
+}
+
+class FilterContainer extends React.Component<FilterContainerProps> {
+    render() {
+        return (
+            <Grid>
+                <Grid.Row>
+                    {this.props.children}
+                </Grid.Row>
+            </Grid>
+        )
+    }
+}
+
+class FilterContainerItem extends React.Component<FilterContainerItemProps> {
+    render() {
+        const { children, ...options } = this.props;
+
+        return (
+            <Grid.Column {...options}>
+                {children}
+            </Grid.Column>
+        )
+    }
+}
+
+class FilterButtonComponent extends React.Component<FilterButtonComponentProps> {
+    static defaultProps = {
+        icon: 'filter'
+    };
+
+    render() {
+        const { icon } = this.props;
+
+        return (
+            <Grid.Column verticalAlign={'bottom'} style={styles.button}>
+                <Button primary icon={icon}/>
+            </Grid.Column>
+        )
+    }
+}
+
 class Filter extends React.Component<FilterProps, {}> {
     static defaultProps = {
         filterNamePrefix: 'filter',
+        filterContainer: FilterContainer,
+        filterContainerItem: FilterContainerItem,
+        filterButtonComponent: FilterButtonComponent,
         filterButton: {
             icon: 'filter',
             visible: true
@@ -54,42 +114,46 @@ class Filter extends React.Component<FilterProps, {}> {
     }
 
     render() {
-        const { items, handleSubmit, filterButton } = this.props;
+        const {
+            items,
+            handleSubmit,
+            filterButton,
+            filterContainer: FilterContainer,
+            filterContainerItem: FilterContainerItem,
+            filterButtonComponent: FilterButtonComponent
+        } = this.props;
 
         return (
             <Form onSubmit={handleSubmit(this.handleSearch)} style={styles.form}>
-                <Grid>
-                    <Grid.Row>
-                        {items.map((item, i) => {
-                            const { type, component, options = {}, updateAfterChange = false, ...props } = item;
-                            let Component;
+                <FilterContainer>
+                    {items.map((item, i) => {
+                        const { type, component, options = {}, updateAfterChange = false, ...props } = item;
+                        let Component;
 
-                            if (component) {
-                                Component = component;
-                            } else {
-                                Component = factory(type);
-                            }
+                        if (component) {
+                            Component = component;
+                        } else {
+                            Component = factory(type);
+                        }
 
-                            if (updateAfterChange) {
-                                props.handleChange = handleSubmit(this.handleSearch);
-                            }
+                        if (updateAfterChange) {
+                            props.handleChange = handleSubmit(this.handleSearch);
+                        }
 
-                            return (
-                                <Grid.Column key={i} width={3} {...options}>
-                                    <Field
-                                        key={i}
-                                        {...props}
-                                        component={Component} />
-                                </Grid.Column>
-                            )
-                        })}
-                        {filterButton.visible && (
-                            <Grid.Column verticalAlign={'bottom'} style={styles.button}>
-                                <Button primary icon={get(filterButton, 'icon', 'filter')}/>
-                            </Grid.Column>
-                        )}
-                    </Grid.Row>
-                </Grid>
+                        return (
+                            <FilterContainerItem key={i} width={3} {...options}>
+                                <Field
+                                    key={i}
+                                    {...props}
+                                    component={Component} />
+                            </FilterContainerItem>
+                        )
+                    })}
+
+                    {filterButton.visible && (
+                        <FilterButtonComponent icon={get(filterButton, 'icon', 'filter')}/>
+                    )}
+                </FilterContainer>
             </Form>
         )
     }
